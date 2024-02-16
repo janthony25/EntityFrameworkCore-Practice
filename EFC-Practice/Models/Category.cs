f9576a34ab35c12f3437b40d4ab411608f62b6e9
janthony25 <junanthony.business@gmail.com>
2024-02-17 08:03:27 +1300

    Add Category DB

---
+++ EFC-Practice/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFC_Practice.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string  Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
